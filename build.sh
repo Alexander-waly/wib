cd4acedc22e1ae1ead617e7b5d676881e85e2362
Zack Rothauser <zrothauser@gmail.com>
2019-02-19 11:54:32 -0600

    Remove installation of PHP and WP

--- build.sh
+++ build.sh
@@ -7,14 +7,6 @@ PHP_PATH=php-$PHP_VERSION
 WORDPRESS_VERSION=5.0.3
 WORDPRESS_PATH=wordpress-$WORDPRESS_VERSION
 
-echo "Get PHP source"
-wget http://downloads.php.net/~cmb/$PHP_PATH.tar.xz
-tar xf $PHP_PATH.tar.xz
-rm $PHP_PATH.tar.xz
-
-echo "Apply patch"
-patch -p0 -i mods.diff
-
 echo "Configure"
 cd $PHP_PATH
 emconfigure ./configure \
@@ -34,12 +26,6 @@ emconfigure ./configure \
   --enable-mbstring \
   --disable-mbregex \
   --enable-tokenizer
-
-echo "Get WordPress source"
-wget https://wordpress.org/$WORDPRESS_PATH.tar.gz
-mkdir -p $WORDPRESS_PATH
-tar xf $WORDPRESS_PATH.tar.gz
-rm $WORDPRESS_PATH.tar.gz
 
 echo "Build"
 emmake make
