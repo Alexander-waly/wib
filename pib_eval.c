422367ec56596e16cb1d6e5c6bdc01cceeedda4e
Jason Stallings <octalmage@gmail.com>
2019-02-19 01:54:53 +0000

    start working on pib_eval.c.

--- pib_eval.c
+++ pib_eval.c
@@ -4,9 +4,12 @@
 
 int EMSCRIPTEN_KEEPALIVE pib_eval(char *code) {
     int ret = 0;
+    zend_error_handling error_handling;
+
+    zend_replace_error_handling(EH_THROW, NULL, &error_handling TSRMLS_CC);
     putenv("USE_ZEND_ALLOC=0");
     PHP_EMBED_START_BLOCK(0, NULL)
-        ret = zend_eval_string(code, NULL, "PIB");
+        ret = zend_eval_string_ex(code, NULL, "WIB", 1);
     PHP_EMBED_END_BLOCK()
     return ret;
 }
